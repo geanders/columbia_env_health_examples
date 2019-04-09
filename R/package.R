8db4ffb42bebe36d40e633831a92dbb919c7e442
Brooke Anderson <brooke.anderson@colostate.edu>
2019-04-08 22:15:53 -0400

    Add a bit of code for packages

--- R/package.R
+++ R/package.R
@@ -4,7 +4,12 @@
 # create a new project for an R package, then move this function 
 # into its "R" directory and go from there. 
 
-
+# Create the function
 c_to_f <- function(temp_in_c){
-  temp_in_c
+  (temp_in_c * 9 / 5) + 32
 }
+
+# Try the function out
+
+c_to_f(0)
+c_to_f(100)
